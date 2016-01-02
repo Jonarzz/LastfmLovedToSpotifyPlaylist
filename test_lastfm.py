6fb35a25846edc5f97351d8bdd84a4a07d6a3a58
Jonasz Czerepko <jonasz.czerepko@gmail.com>
2016-01-02 11:27:11 +0100

    Expanding tests for lastfm module.

--- test_lastfm.py
+++ test_lastfm.py
@@ -17,13 +17,17 @@ class LastfmTest(unittest.TestCase):
         """Method testing the connection to the LastFM API and to the given account."""
         user = lastfm.get_lastfm_user('dummy_test_acc', 'test1!')
 
-        self.assertEqual(user.get_name(), 'dummy_test_acc')
-        self.assertEqual(user.get_playcount(), 0)
+        name = user.get_name()
+        playcount = user.get_playcount()
+
+        self.assertEqual(name, 'dummy_test_acc')
+        self.assertEqual(playcount, 0)
 
     def test_get_loved_tracks(self):
         """Method testing get_loved_tracks method of lastfm module - tests if the returned value
         is not None."""
-        self.assertIsNotNone(lastfm.get_loved_tracks('dummy_test_acc', 'test1!'))
+        loved_tracks = lastfm.get_loved_tracks('dummy_test_acc', 'test1!')
+        self.assertIsNotNone(loved_tracks)
 
     def test_create_track_dict(self):
         """Method testing create_track_dict method of lastfm module - tests if for the mocked
@@ -32,13 +36,18 @@ class LastfmTest(unittest.TestCase):
             track_artist = 'test{}_artist'.format(i)
             track_title = 'test{}_track'.format(i)
             expected_dict = {'artist': track_artist, 'title': track_title}
+
             track = pylast.Track(track_artist, track_title, None)
             loved_track = pylast.LovedTrack(track, None, None)
-            self.assertEqual(lastfm.create_track_dict(loved_track), expected_dict)
+
+            output_dict = lastfm.create_track_dict(loved_track)
+
+            self.assertEqual(output_dict, expected_dict)
 
     def test_get_loved_tracks_list(self):
         """Method testing get_loved_tracks method which retrives loved tracks for a given user
         and creates a list of dictionaries."""
-        self.assertEqual(lastfm.get_loved_tracks('dummy_test_acc', 'test1!'),
+        loved_tracks_list = lastfm.get_loved_tracks('dummy_test_acc', 'test1!')
+        self.assertEqual(loved_tracks_list,
                          [{'artist': 'Freddy The Flying Dutchman & The Sistina Band',
                            'title': 'Wojtyla Disco Dance'}])
