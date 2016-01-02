4aa410a3c67b975f432c8546584e89b27135890c
Jonasz Czerepko <jonasz.czerepko@gmail.com>
2016-01-02 21:19:48 +0100

    Set environmental variables in properties.

--- properties.py
+++ properties.py
@@ -1,17 +1,19 @@
 """File containing properties for the LastfmLovedToSpotifyPlaylist project."""
 
+import os
+
 __author__ = 'Jonarzz'
 
 PORT = 80
 
-LASTFM_API_KEY = ''
-LASTFM_API_SECRET = ''
+LASTFM_API_KEY = os.environ.get('LASTFM_API_KEY')
+LASTFM_API_SECRET = os.environ.get('LASTFM_API_SECRET')
 
-SPOTIFY_API_ID = ''
-SPOTIFY_API_SECRET = ''
-SPOTIFY_REDIRECT_URL = '{}'.format(PORT)
+SPOTIFY_API_ID = os.environ.get('SPOTIFY_API_ID')
+SPOTIFY_API_SECRET = os.environ.get('SPOTIFY_API_SECRET')
+SPOTIFY_REDIRECT_URL = str(os.environ.get('SPOTIFY_REDIRECT_URL')).format(PORT)
 
-MODIFY_PLAYLISTS_SCOPE = 'playlist-modify-public'
+MODIFY_PLAYLISTS_SCOPE = 'playlist-modify-public playlist-modify-private'
 
 TITLE_ENDINGS_TO_CUT = [' ft. ', ' ft ', ' feat ', 'feat. ', '(']
 
